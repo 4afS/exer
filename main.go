0eb50281b32f4fce1f7b5ffe29c3e88878b7409b
4afS <4afs.h1@gmail.com>
2020-01-06 20:51:12 +0900

    define skeleton

--- main.go
+++ main.go
@@ -1,8 +1,54 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	var (
+		runOption   = flag.Bool("run", false, "Run a project you're in.")
+		buildOption = flag.Bool("build", false, "Run a project you're in.")
+	)
+
+	flag.Parse()
+
+	path, err := projectRootPath()
+
+	if err != nil {
+	}
+
+	switch {
+	case *runOption && *buildOption:
+		usage()
+	case *runOption:
+		run(path)
+	case *buildOption:
+		build(path)
+	default:
+		usage()
+	}
+}
+
+func usage() {
+	fmt.Printf(`Usage of exer:
+  -build
+      Run a project you're in.
+  -run
+      Run a project you're in.
+`)
+	os.Exit(1)
+}
+
+func projectRootPath() (string, error) {
+	return "", nil
+}
+
+func run(path string) error {
+	return nil
+}
+
+func build(path string) error {
+	return nil
 }
